51d96cb7c0d058ad273ea3a8d19242a99f84226d
fp12 <poupi.12.pi+github@gmail.com>
2016-07-12 18:14:07 -0400

    Heroku test

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hello World')
+import aiohttp
+import asyncio
+import os
+
+CHALLONGE_API_URL = "api.challonge.com/v1"
+url = "https://%s/tournaments.xml" % (CHALLONGE_API_URL)
+
+params = {'tournament[name]': 'testmyherokusetup', 'tournament[url]': 'test_1', 'tournament[tournament_type]': 'swiss'}
+"""
+with aiohttp.ClientSession() as session:
+    async with session.post(url, params=params, auth=aiohttp.BasicAuth('fp12', os.getenv('challonge_apikey', ''))) as resp:
+        print(resp.status)
+        print(await resp.text())
+
+async with EXPR as VAR:
+    BLOCK
+"""
+with aiohttp.ClientSession() as session:
+    mgr = session.post(url, params=params, auth=aiohttp.BasicAuth('fp12', os.getenv('challonge_apikey', '')))
+    aexit = type(mgr).__aexit__
+    aenter = type(mgr).__aenter__(mgr)
+    exc = True
+
+    resp = aenter
+    aexit(mgr, None, None, None)
